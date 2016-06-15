e666652faa1408b385f72234b3f3f5da5c25e883
justin <jyang772@gmail.com>
2016-06-14 21:29:05 -0400

    hw1

--- hw1/Intersection.java
+++ hw1/Intersection.java
@@ -17,7 +17,7 @@ public class Intersection {
 	    }
 
 	    {
-	    int[] x = {2, 5, 3, 7,6172,9818,5147,8699,4632};
+	    int[] x = {2, 5, 3, 7};
 	    System.out.println("x = " + Arrays.toString(x));
 	    int[] y = {5, 2, 9, 0, 1};
 	    System.out.println("y = " + Arrays.toString(y));
@@ -30,7 +30,7 @@ public class Intersection {
 	    System.out.println(Arrays.equals(z, correct));
 	    }
 	}
-
+	
 	static int[] intersection(int[] nums1, int[] nums2) {
 
 		ArrayList<Integer> dup = new ArrayList<Integer>();
@@ -38,22 +38,45 @@ public class Intersection {
 		Arrays.sort(nums1);
 		Arrays.sort(nums2);	
 	
-		int k=0;
-		for(int i=0; i<nums1.length; i++) { 		//Check for duplicates in first array
-			if((i==0) || (nums1[i] != nums1[i-1]))	//Take advantage of short-ciruit evaluation
-				for(int j=k; j<nums2.length; j++) {
-					if(nums1[i] == nums2[j]){
-						if((j==0 || i==0) || ((nums2[j-1] != nums2[j])))  //Check for duplicates in result
-							dup.add(nums1[i]);
-						k=j+1;		//Don't check [j] < current	
-					}
+		int i=0,j=0;	//Improved! O(nlogn) runtime  
+		int dp = Integer.MAX_VALUE;
+		while(true) {	
+			if((i >= nums1.length) || (j >= nums2.length)) {
+				break;		
+			}
+			if(nums1[i] == nums2[j]) { //Take advantage of short-circuit evaluation
+				if((i==0 || j==0) || ((nums1[i] != dp) && (nums2[j] != dp))) {
+					//if((i==0 || j==0) || (nums1[i] != nums1[i-1]) || (nums2[j] != nums2[j-1]))
+					dup.add(nums1[i]);
+					dp = nums1[i];
 				}
+				i++;
+				j++;
+			}
+			else if(nums1[i] > nums2[j])
+				j++;
+			else
+				i++;
 		}
-	
+				
+		/*      First version, slightly better than O(n^2), still required peeking back one to check for duplicate :(
+                int k=0;
+                for(int i=0; i<nums1.length; i++) {             //Check for duplicates in first array
+                        if((i==0) || (nums1[i] != nums1[i-1]))  //Take advantage of short-ciruit evaluation
+                                for(int j=k; j<nums2.length; j++) {
+                                        if(nums1[i] == nums2[j]){
+                                                if((j==0 || i==0) || ((nums2[j-1] != nums2[j])))  //Check for duplicates in result
+                                                        dup.add(nums1[i]);
+                                                k=j+1;          //Don't check [j] < current     
+                                        }
+                                }
+                }
+*/  
+
 		//Convert ArrayList<Integer> to int[]
 		Iterator<Integer> iterator = dup.iterator();
 		int[] ret = new int[dup.size()];
-    		for (int i = 0; i < ret.length; i++)
+    		for (i = 0; i < ret.length; i++)
     		{
         		ret[i] = iterator.next().intValue();
     		}
--- hw1/O.java
+++ hw1/O.java
@@ -10,8 +10,8 @@ public class O
     public void call(int x){
         O o = new O();
         this.x = 5;
-        O s = new O();   
-	s.x = 10;
+        //O s = new O();   
+	//s.x = 10;
 	System.out.println("Output: " + x);
         System.out.println("Output: " + O.x);
         System.out.println("Output: " + o.x);
